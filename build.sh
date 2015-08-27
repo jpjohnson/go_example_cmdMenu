654869d821e577b4c3f975e64898db626ff02011
jpjohnson <johnpjohnson@gmail.com>
2015-08-27 15:36:13 -0400

    build for ubuntu

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -it --rm -v $GOPATH/src/github.com/jpjohnson/go_example_cmdMenu -e GOOS=linux -e GOARCH=arm ubuntu `go build -v -o go_example_cmdMenu`
+docker run -it --rm -v $GOPATH/src/github.com/jpjohnson/go_example_cmdMenu -e GOOS=linux -e GOARCH=amd64 ubuntu `env GOOS=linux GOARCH=amd64 go build -v -o go_example_cmdMenu`
